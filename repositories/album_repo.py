2d0387f4cfe17c8d79941b472f371029b4e0e613
Daniel FCS <danielf.cs@outlook.com>
2023-04-25 23:16:07 +0100

    did I test it? no, does it work? don't know, did I do it? I hope so

--- repositories/album_repo.py
+++ repositories/album_repo.py
@@ -1,17 +1,16 @@
 from db.run_sql import run_sql
 from models.album import Album
-from models.artist import Artist
-import repositories.artist_repo
+import repositories.artist_repo as artist_repo
 
 
 # CRUD
 
 # Create / insert instance in table. This will receive a full Album() class object, without the ID.
 def insert(album):
-    sql = 'INSERT INTO albums (title,genre,artist) VALUES (%s,%s,%s) RETURNING *'
+    sql = 'INSERT INTO albums (title,genre,artist_id) VALUES (%s,%s,%s) RETURNING *'
              # ^command    ^table   ^columns         ^to add    ^placeholder  ^return the row (for ID(?))
-    values = [album.title,album.genre,album.artist]
-
+    values = [album.title,album.genre,album.artist.id]
+    #album.artist.id is fetching the id of the artist since it is an object class with an id attribute
     # at this point we can run the SQL and place these values in the respective table/columns
     # this point will generate the ID
     row = run_sql(sql,values)
@@ -39,5 +38,32 @@ def select_all(id):
     if row:
         #if row == [] then it's falsy
         #from here we need to create the task class object, but we need to make the artist repo first so we can pull the artist which goes in to album.artist
-        #I totally did not start from here and now need to go do the artist :)
-        print('got a row watchugonndoboutit')
+        row_data = [0]
+        #initially row is [[]] so we need to step into in order to get the information
+        artist = artist_repo.select(row_data['id'])
+        #we then fetch the respective artist so that it can be put as a parameter of Album
+        album = Album(row_data['title'],row_data['genre'],artist,row_data['id'])
+        #lastly we assign the respective row value to the parameter by calling the key
+        return album
+    
+#Update / find the thing, change the thing
+def update_one(album):
+    sql = 'UPDATE albums SET (title,genre,artist) = (%s, %s, %s) WHERE id = %s'
+    values = [album.title,album.genre,album.artist,album.id]
+    run_sql(sql,values)
+
+def update_all(album):
+    sql = 'UPDATE albums SET (title,genre,artist) = (%s,%s,%s)'
+    values = [album.title,album.genre,album.artist]
+    run_sql(sql,values)
+
+#Delete - make it go away
+
+def delete_one(id):
+    sql = 'DELETE FROM albums WHERE id = %s'
+    values = [id]
+    run_sql(sql,values)
+
+def delete_all():
+    sql = 'DELETE * FROM albums'
+    run_sql(sql)
